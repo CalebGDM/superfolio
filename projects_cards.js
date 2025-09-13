ea82cfc5acbecc933b6558bd8634bcca8128b445
Caleb Garcia <cgarcia85@ucol.mx>
2025-09-13 09:17:48 -0600

    feat: Unified and improved Projects srceens script

--- projects_cards.js
+++ projects_cards.js
@@ -1,12 +1,8 @@
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
-    if (projectContainer) {
-      console.log(data);
-      showProjects(data);
-    } else {
-      showProjectsHome(data);
-    }
+    showProjectsHome(data);
+    
   });
 
 const projectHomeContainer = document.getElementById("project-home-container");
@@ -27,26 +23,3 @@ const showProjectsHome = (data) => {
   });
 };
 
-const projectContainer = document.getElementById("project-container");
-const showProjects = (data) => {
-  data.projects.forEach((project) => {
-    const card = document.createElement("article");
-    card.className = "col";
-    card.innerHTML = `
-              <div 
-                class="text-decoration-none text-dark card h-100 project-card"
-                data-bs-toggle="modal"
-                data-bs-target="#projectModal"
-                data-project-id="${project.id}"
-              
-              >
-                <img src="${project.image}" class="card-img-top" alt="${project.title}" />
-                <div class="p-3">
-                  <h3 class="fs-4">${project.title}</h3>
-                  <p>${project.shortDescription}</p>
-                </div>
-              </div>
-            `;
-    projectContainer.appendChild(card);
-  });
-};
